669dbaa87c3b72f28da1418763fda75abffa5c2a
PashaPodolsky <ppodolsky@me.com>
2016-02-03 00:48:53 +0300

    made plotting optional

--- combiner_pymc.py
+++ combiner_pymc.py
@@ -10,6 +10,7 @@ import matplotlib.ticker
 import matplotlib.pyplot as plt
 import pickle
 import random
+import time
 
 import PyGammaCombo
 tstr = PyGammaCombo.TString
@@ -39,6 +40,7 @@ ap.add_argument('-b', action='store', default=5000, type=int, help='Number of bu
 ap.add_argument('-c', action='store', required=True, type=int, help='Number of the combination')
 ap.add_argument('-bins', action='store_true', help='If set, then output as histrograms')
 ap.add_argument('-i', action='store_true', help='Print information about combiner')
+ap.add_argument('-plot', action='store_true', help='Plot bars and graphs')
 ap.add_argument('-u', action='store_true', help='Switch combination function to uniform')
 ap.add_argument('-vars', nargs='+', required=True, help='Variables')
 args = vars(ap.parse_args())
@@ -49,6 +51,7 @@ combination = args['c']
 print_information = args['i']
 is_mcmc = not args['u']
 bins = args['bins']
+plot_graph = args['plot']
 burnout = args['b']
 desired_variables, lower_bounds, upper_bounds, edges = parse_dv(args['vars'], bins)
 
@@ -60,6 +63,7 @@ extract = PyGammaCombo.gammacombo_utils.extractFromRooArgSet
 toRooRealVar = PyGammaCombo.gammacombo_utils.toRooRealVar
 
 if __name__ == "__main__":
+    start_time = time.time()
     gce = PyGammaCombo.gammacombo_utils.getGammaComboEngine("")
     cmb = gce.getCombiner(combination)
     cmb.combine()
@@ -91,7 +95,6 @@ if __name__ == "__main__":
                     dbmode='w',
                     dbname='mcmc-{}_combiner.pickle'.format(combination))
         mcmc.sample(iter=N, burn=burnout, thin=1)
-
         # Output
         data = {v: mcmc.trace(v)[:] for v in desired_variables}
         if bins:
@@ -103,8 +106,9 @@ if __name__ == "__main__":
         else:
             with gzip.open('output/raw.dat.gz', 'w') as file:
                 pickle.dump({'data': data}, file, protocol=2)
-        for v in desired_variables:
-            commons.plot(data[v], combination, v)
+        if plot_graph:
+            for v in desired_variables:
+                commons.plot(data[v], combination, v)
 
     else: #not MCMC
         var_dict = {}
@@ -121,12 +125,15 @@ if __name__ == "__main__":
                 data_to_save = {}
                 for v in data:
                     data_to_save[v] = np.histogram(data[v], bins=edges[v], weights=weights)
-                    hist, bin_edges = data_to_save[v]
-                    plt.bar(bin_edges[:-1], hist, width=(max(bin_edges) - min(bin_edges))/len(bin_edges))
-                    plt.xlim(min(bin_edges), max(bin_edges))
-                    plt.savefig('output/{}_{}.png'.format(combination, v))
-                    plt.clf()
+                    if plot_graph:
+                        hist, bin_edges = data_to_save[v]
+                        plt.bar(bin_edges[:-1], hist, width=(max(bin_edges) - min(bin_edges))/len(bin_edges))
+                        plt.xlim(min(bin_edges), max(bin_edges))
+                        plt.savefig('output/{}_{}.png'.format(combination, v))
+                        plt.clf()
                 pickle.dump({'data': data_to_save}, file, protocol=2)
         else:
             with gzip.open('output/raw.dat.gz', 'w') as file:
                 pickle.dump({'data': data, 'weights': weights}, file, protocol=2)
+
+    print("Total time: {}".format(time.time() - start_time))
